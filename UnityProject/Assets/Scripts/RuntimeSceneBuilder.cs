927b6dc73c70e3cfa0c04ffcc7135e97aae8b792
antonbot <Dotasymphony@gmail.com>
2026-03-14 06:53:21 +0000

    FIX: scene guardian targeted hud exact variable path (local)

--- UnityProject/Assets/Scripts/RuntimeSceneBuilder.cs
+++ UnityProject/Assets/Scripts/RuntimeSceneBuilder.cs
@@ -129,18 +129,18 @@ public sealed class RuntimeSceneBuilder : MonoBehaviour
         cam.nearClipPlane = 0.3f;
         cam.farClipPlane = 140f;
 
-        var rightHudCanvas = GameObject.Find("rightHudCanvas")
-                           ?? GameObject.Find("UICanvas")
-                           ?? GameObject.Find("uiCanvas")
-                           ?? GameObject.Find("DebugCanvas");
-        if (rightHudCanvas != null)
+        var rightHudCanvasGo = GameObject.Find("rightHudCanvas")
+                             ?? GameObject.Find("UICanvas")
+                             ?? GameObject.Find("uiCanvas")
+                             ?? GameObject.Find("DebugCanvas");
+        if (rightHudCanvasGo != null)
         {
-            rightHudCanvas.transform.position = new Vector3(60.00f, 0.00f, 0.00f);
-            rightHudCanvas.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            rightHudCanvasGo.transform.position = new Vector3(60.00f, 0.00f, 0.00f);
+            rightHudCanvasGo.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
 
-            var hudCanvas = rightHudCanvas.GetComponent<Canvas>();
-            if (hudCanvas != null)
-                hudCanvas.enabled = false;
+            var rightHudCanvas = rightHudCanvasGo.GetComponent<Canvas>();
+            if (rightHudCanvas != null)
+                rightHudCanvas.enabled = false;
         }
     }
 
