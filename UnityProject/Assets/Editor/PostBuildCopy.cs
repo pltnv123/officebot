37dc485f2a3e834eab307162a1873168c62e89f0
antonbot <Dotasymphony@gmail.com>
2026-03-21 00:22:35 +0000

    fix: add missing usings to resolve compiler errors (AmbientMode, PostProcessBuild)

--- UnityProject/Assets/Editor/PostBuildCopy.cs
+++ UnityProject/Assets/Editor/PostBuildCopy.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.Build;
 using UnityEngine;
 using System.IO;
 
--- UnityProject/Assets/Scripts/UIBridge/WebSocketStateClient.cs
+++ UnityProject/Assets/Scripts/UIBridge/WebSocketStateClient.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Rendering;
 
 namespace OfficeHub.UIBridge
 {
